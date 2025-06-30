1723673ec9f910042c1d63760a1e2c82b945f350
bcchang83 <54743478+bcchang83@users.noreply.github.com>
2025-06-30 12:09:42 +0200

    fix error on streamlit deploy

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+# from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
-from langchain.document_loaders import PyPDFLoader
-from langchain.vectorstores import FAISS
-from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.document_loaders import PyPDFLoader
+# from langchain.vectorstores import FAISS
+# from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.chat_models import ChatOpenAI
+# from langchain_community.chains import ConversationalRetrievalChain
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
 import tempfile
 import os
 
